16db02aa0d4719f578cec43c9d07b3259c1c9f1b
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-06-30 21:07:27 +0600

    27-4 Basic Router Setup

---
+++ src/routes/routes.tsx
@@ -0,0 +1,11 @@
+import { RouterProviderProps, createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+
+const router: RouterProviderProps['router'] = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+
+export default router;

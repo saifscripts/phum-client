4ec27ab786040a7fb5171b8f1680a7b4d4241f44
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-07-04 09:06:33 +0600

    28-4 Create AuthSlice And AuthApi Layer By Injecting In BaseApi

---
+++ src/redux/features/auth/authSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export interface IAuthState {
+  user: null | object;
+  token: null | string;
+}
+
+const initialState: IAuthState = {
+  user: null,
+  token: null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    logOut: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+  },
+});
+
+export const { setUser, logOut } = authSlice.actions;
+export default authSlice.reducer;

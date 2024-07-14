bf114c70344f175930b7bbd40c771c74feee8ff0
Md Saif Ullah <mdsaifullah1302@gmail.com>
2024-07-15 04:10:26 +0600

    31-4 Handle Create Academic Semester Form

--- src/pages/admin/AcademicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/AcademicManagement/CreateAcademicSemester.tsx
@@ -4,13 +4,44 @@ import PHForm from '../../../components/form/PHForm';
 import PHSelect from '../../../components/form/PHSelect';
 
 const nameOptions = [
-  { value: 'Autumn', label: 'Autumn' },
-  { value: 'Summer', label: 'Summer' },
-  { value: 'Fall', label: 'Fall' },
+  { value: '01', label: 'Autumn' },
+  { value: '02', label: 'Summer' },
+  { value: '03', label: 'Fall' },
+];
+
+const currentYear = new Date().getFullYear();
+
+const yearOptions = Array(10)
+  .fill(null)
+  .map((_item, index) => {
+    const year = String(currentYear + index);
+
+    return {
+      value: year,
+      label: year,
+    };
+  });
+
+const monthOptions = [
+  { value: 'January', label: 'January' },
+  { value: 'February', label: 'February' },
+  { value: 'March', label: 'March' },
+  { value: 'April', label: 'April' },
+  { value: 'May', label: 'May' },
+  { value: 'June', label: 'June' },
+  { value: 'July', label: 'July' },
+  { value: 'August', label: 'August' },
+  { value: 'September', label: 'September' },
+  { value: 'October', label: 'October' },
+  { value: 'November', label: 'November' },
+  { value: 'December', label: 'December' },
 ];
 
 const CreateAcademicSemester = () => {
   const onSubmit = (data: FieldValues) => {
+    data.code = data.name;
+    data.name = nameOptions[Number(data.name) - 1].label;
+
     console.log(data);
   };
   return (
@@ -18,6 +49,17 @@ const CreateAcademicSemester = () => {
       <Col span={8}>
         <PHForm onSubmit={onSubmit}>
           <PHSelect name={'name'} label="Name" options={nameOptions} />
+          <PHSelect name={'year'} label="Year" options={yearOptions} />
+          <PHSelect
+            name={'startMonth'}
+            label="Start Month"
+            options={monthOptions}
+          />
+          <PHSelect
+            name={'endMonth'}
+            label="End Month"
+            options={monthOptions}
+          />
           <Form.Item>
             <Button style={{ width: '100%' }} type="primary" htmlType="submit">
               Submit
